ae489b9ee99e3ded0014460966515ccd9af36e0f
MeldaForero <meldasakar@gmail.com>
2024-04-25 15:41:07 -0500

    changes2

--- test/test.java
+++ test/test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test test");
         System.out.println("one");
+        System.out.println("555");
     }
 
 }
